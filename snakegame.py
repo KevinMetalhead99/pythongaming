adc93abb51e8f47e1244c10ef6725dc47f0e6772
kevinPython <phonehtetmyint792@gmail.com>
2021-01-06 10:21:03 +0630

    Second commit Kevin .. Change list clear to break

--- snakegame.py
+++ snakegame.py
@@ -1,2 +1,20 @@
 import pygame
 import time
+import random
+
+pygame.init()
+snake =[]
+position = (0,0)
+snake.append(position)
+for i in snake:
+    print(len(snake))
+    print(i)
+    x = round(random.randrange(0,100))
+    y = round(random.randrange(0,100))
+    if(len(snake) > 5):
+        snake.clear()
+    position = (x,y)
+    snake.append(position)
+@staticmethod
+def snakeMove(window):
+    pass
