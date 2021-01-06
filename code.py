1020b7e87e5dbe0eb4c3b79046affb2564c660ea
Phone Htet Myint <phonehtetmyint792@gmail.com>
2021-01-06 18:19:03 +0630

    This is in THS commit

--- code.py
+++ code.py
@@ -1,1 +1,16 @@
-print ("This is second day")
+import random
+
+position = (0,0)
+snake = []
+snake.append(position)
+
+for i in snake:
+    print(i)
+    if(len(snake) > 5):
+        break
+    x = round(random.randrange(0,100))
+    y = round(random.randrange(0,100))
+    position = (x,y)
+    snake.append(position)
+    
+
